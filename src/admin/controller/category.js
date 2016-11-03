0e5535c521aa8c426027d892e4aad3df6531936b
username <977865769@qq.com>
2016-11-03 23:01:38 +0800

    update

--- src/admin/controller/category.js
+++ src/admin/controller/category.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import Base from './base.js';
+import moment from 'moment';
 
 export default class extends Base {
     async categoryAction(){
@@ -10,5 +11,12 @@ export default class extends Base {
             'categories': categories
         });
         return this.display();
+    }
+    async addcategoryAction(){
+        let name=this.http.post("name");
+        let model=this.model('t_category');
+        let insertDate=moment().format("YYYY-MM-DD HH:mm:ss");
+        let insertId=await model.addCategory(name,insertDate);
+        return this.success(insertId);
     }
 }
--- src/admin/model/t_category.js
+++ src/admin/model/t_category.js
@@ -3,4 +3,7 @@ export default class extends think.model.base {
     getAllCategories(){
         return this.select();
     }
+    addCategory(name,insertDate){
+      return this.add({Name:name,CreatedDate:insertDate,UpdatedDate:insertDate,Sort:0,ParentId:0});
+    }
 }
