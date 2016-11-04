d7bcd1d76ad256f3266fdc932a955bf326131d3c
Administrator <977865769@qq.com>
2016-11-04 17:27:35 +0800

    update

--- src/admin/controller/category.js
+++ src/admin/controller/category.js
@@ -19,4 +19,19 @@ export default class extends Base {
         let insertId=await model.addCategory(name,insertDate);
         return this.success(insertId);
     }
+
+    async editcategoryAction(){
+        let name=this.http.post("name");
+        let id=this.http.post("id");
+        let model=this.model("t_category");
+        let updatedDate=moment().format("YYYY-MM-DD HH:mm:ss");
+        let count= model.where({Id:id}).update({Name:name,UpdatedDate:updatedDate});
+        return this.success(count);
+    }
+    async delcategoryAction() {
+        let id=this.http.post("id");
+        let model=this.model("t_category");
+        let count=model.where({Id:id}).delete();
+        return this.success(count);
+    }
 }
--- www/static/admin/js/main.js
+++ www/static/admin/js/main.js
@@ -1,4 +1,7 @@
 $(function(){
    $(document).pjax('a', '#content-main');
    $.pjax({url: '/admin/main/main', container: '#content-main'});
+   $("#small-chat").on("click",function(){
+      $.pjax.reload("#content-main");
+   });
 });
