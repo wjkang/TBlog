aa481cbc4847269ba8d1ae574e8d8033b357a077
Administrator <977865769@qq.com>
2016-11-11 17:59:28 +0800

    update

--- src/common/bootstrap/global.js
+++ src/common/bootstrap/global.js
@@ -10,6 +10,38 @@
  *     
  * }
  */
-global.formatDate=function(date){
-   return date.getFullYear()+'-'+date.getMonth()+'-'+date.getDate() +' '+date.getHours()+':'+date.getMinutes()+':'+date.getSeconds();
+global.formatDate=function (d, sec) {
+   var time;
+   var date = new Date(d);
+   var y = date.getFullYear();
+   var M = date.getMonth() + 1;
+   M = M < 10 ? "0" + M : M;
+   var d = date.getDate();
+   d = d < 10 ? "0" + d : d;
+   var h = date.getHours();
+   h = h < 10 ? "0" + h : h;
+   var m = date.getMinutes();
+   m = m < 10 ? "0" + m : m;
+   var s = date.getSeconds();
+   s = s < 10 ? "0" + s : s;
+   if (sec) {
+      time = y + "-" + M + "-" + d + " " + h + ":" + m + ":" + s;
+   } else {
+      time = y + "-" + M + "-" + d + " " + h + ":" + m;
+   }
+   return time;
+}
+global.formatDate1=function (d) {
+   var time;
+   var date = new Date(d);
+   var y = date.getFullYear();
+   var M = date.getMonth() + 1;
+   M = M < 10 ? "0" + M : M;
+   var d = date.getDate();
+   d = d < 10 ? "0" + d : d;
+   time = y + "年" + M + "月" + d + "日";
+   return time;
+}
+global.delHtmlTag=function(html){
+   return html.replace(/<[^>]+>/g,"")
 }
